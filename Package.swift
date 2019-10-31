349a9126a9819bf201008bbbb583acaafafbd496
watanabe <watanabe@vcube.co.jp>
2019-10-31 16:51:47 +0900

    Add platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftFilePath",
-
+    platforms: [.iOS(.v9), .macOS(.v10_10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
