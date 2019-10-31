8ff2b7c688955d20ad291439133b52750599c0ce
watanabe <watanabe@vcube.co.jp>
2019-10-31 16:31:23 +0900

    import Foundation for Swift Package Manager
    import Foundation for Swift Package Manager

--- SwiftFilePath/PathExtensionDir.swift
+++ SwiftFilePath/PathExtensionDir.swift
@@ -5,7 +5,7 @@
 //  Created by nori0620 on 2015/01/08.
 //  Copyright (c) 2015年 Norihiro Sakamoto. All rights reserved.
 //
-
+import Foundation
 // Instance Factories for accessing to readable iOS dirs.
 #if os(iOS)
 extension Path {
